7aa4bad5030e0d0d14c87229773c8432fd4903d6
vikuska456 <34243813+vikuska456@users.noreply.github.com>
2017-12-13 13:59:32 +0000

    Website Added
    Website Added

---
+++ campcanada/js/textEnlarge.js
@@ -0,0 +1,8 @@
+
+  function textEnlarge() {
+  document.getElementById("intro").style.fontSize ="xx-large";
+}
+
+function textReduce() {
+  document.getElementById("intro").style.fontSize = "15pt";
+}
